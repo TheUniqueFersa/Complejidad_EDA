996f21725f9a876bdf557bdaab0b43f8f98572ff
TheUniqueFersa <holafersa@gmail.com>
2024-09-15 22:05:10 -0600

    feat(Quick): implementa correctamente la subclase QuickSort

--- src/Principal.java
+++ src/Principal.java
@@ -18,11 +18,13 @@ public class Principal {
         Seleccion selection = new Seleccion(arr);
         Insercion insertion = new Insercion(arr);
         Heap heap = new Heap(arr);
+        Quick quick = new Quick(arr);
         for(int i = 0; i < n; i++){
             bubble.calcularOperaciones();
             selection.calcularOperaciones();
             insertion.calcularOperaciones();
             heap.calcularOperaciones();
+            quick.calcularOperaciones();
 
             Utilerias.randomArr(arr, tam);
             Utilerias.imprimirArreglo(arr);
@@ -30,6 +32,7 @@ public class Principal {
             selection.inicializarArr(arr);
             insertion.inicializarArr(arr);
             heap.inicializarArr(arr);
+            quick.inicializarArr(arr);
         }
         System.out.println("--- BubbleSort ---");
         for(Resultado r: bubble.resultados){
@@ -48,6 +51,11 @@ public class Principal {
         }
         System.out.println("--- HeapSort ---");
         for(Resultado r: heap.resultados){
+            r.mostrarResultados();
+            System.out.println();
+        }
+        System.out.println("--- QuickSort ---");
+        for(Resultado r: quick.resultados){
             r.mostrarResultados();
             System.out.println();
         }
---
+++ src/Quick.java
@@ -0,0 +1,78 @@
+
+public class Quick extends AlgoritmoOrdenamiento{
+    public Quick(int[] arr){
+        super(arr);
+    }
+    protected Resultado sort() {
+        QuickSort(0, this.arr.length-1);
+        Resultado res = new Resultado("QuickSort", operaciones, comparaciones, intercambios, inserciones, arr, arr.length);
+        resultados.add(res);
+        return res;
+    }
+    private int partition(int low, int high){
+        int pivot = arr[high];
+        operaciones+=2;
+        int i = (low-1);
+        operaciones+=2;
+
+        operaciones+=2;
+        for (int j=low; j<high; j++, operaciones+=3){ 
+            operaciones+=2;
+            comparaciones++;
+            if (arr[j] <= pivot){
+                operaciones+=2;
+                i++;
+                operaciones+=7;
+                intercambios++;
+                Utilerias.intercambiar(arr, i,j);
+            }
+        }
+        int temp = arr[i+1];
+        operaciones+=3;
+
+        arr[i+1] = arr[high];
+        operaciones+=4;
+
+        arr[high] = temp;
+        operaciones+=2;
+
+        operaciones++;
+        return i+1;
+    }
+    private void QuickSort(int low, int high){
+        operaciones++;
+        if (low < high){
+            operaciones++;
+            int pi = partition(low, high);
+            operaciones++;
+            QuickSort(low, pi-1);
+
+            operaciones++;
+            QuickSort(pi+1, high);
+        }
+    }
+
+    //STATIC
+    public static int partition(int arr[], int low, int high){
+        int pivot = arr[high];
+        int i = (low-1);
+        for (int j=low; j<high; j++){ 
+            if (arr[j] <= pivot){
+                i++;
+                Utilerias.intercambiar(arr, i,j);
+            }
+        }
+        int temp = arr[i+1];
+        arr[i+1] = arr[high];
+        arr[high] = temp;
+        return i+1;
+    }
+
+    public static void sort(int arr[], int low, int high){
+        if (low < high){
+            int pi = partition(arr, low, high);
+            sort(arr, low, pi-1);
+            sort(arr, pi+1, high);
+        }
+    }
+}
